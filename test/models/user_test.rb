b1f1d90dc1887c5cccdca7e144601be74a3310cd
Vishal Agarwal <agarwalvishal1994@gmail.com>
2016-06-19 18:30:54 +0000

    Add user microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -66,4 +66,12 @@ class UserTest < ActiveSupport::TestCase
     # @user doesn't exist in database
     assert_not @user.authenticated?(:remember, '') 
   end
+  
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 end
